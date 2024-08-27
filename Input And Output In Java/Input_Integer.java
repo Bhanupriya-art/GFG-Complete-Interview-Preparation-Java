7304b899cdb20ca885244077f34d6c1f392e5957
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-08-27 19:53:41 +0530

    Create Input_Integer.java

---
+++ Input And Output In Java/Input_Integer.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            
+// } Driver Code Ends
+//User function Template for Java
+
+//Scanner scn=new Scanner(System.in);
+//Complete the code
+int myInput= scn.nextInt();
+//Complete the code
+System.out.println(myInput + 10);
+//scn.close();
+
+
+//{ Driver Code Starts.
+        }
+        scn.close();
+    }
+}
+// } Driver Code Ends
