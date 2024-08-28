1e9523a95a1f46993379c8ee64b5b0e87b4ec503
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-08-28 13:04:29 +0530

    Create Input_String.java

---
+++ Input And Output In Java/Input_String.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0) {
+            
+// } Driver Code Ends
+//User function Template for Java
+
+// BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+//Complete the below code
+        
+String myInput = read.readLine();
+        
+//Complete the below code
+    
+System.out.println(myInput);
+
+//read.close();
+
+
+
+//{ Driver Code Starts.
+        }
+    }
+}
+// } Driver Code Ends
