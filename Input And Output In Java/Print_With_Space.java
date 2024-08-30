b405ccff3d00c734296e74217ac00df677d6325a
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-08-30 15:14:51 +0530

    Create Print_With_Space.java

---
+++ Input And Output In Java/Print_With_Space.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+//User function Template for Java
+
+public static void utility(String a, String b){
+
+    //Write your code below that prints a <space> b
+    System.out.println( a +" "+b );
+    
+    
+    //Write your code below that prints a <space> b
+
+}
+
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        scn.nextLine();
+        while(t-- > 0) {
+            String a = scn.nextLine();
+            String b = scn.nextLine();
+            utility(a, b);
+        }
+    }
+}
+
+// } Driver Code Ends
