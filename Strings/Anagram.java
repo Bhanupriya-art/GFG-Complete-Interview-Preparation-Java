70bcca5b11339ba3a2f0613747e565bedb3f9767
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-11-06 19:54:19 +0530

    Create Anagram.java

---
+++ Strings/Anagram.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        if(a.length() != b.length()){
+            return false;
+        }
+        
+        char a1[] = a.toCharArray();
+        Arrays.sort(a1);
+        a = new String(a1);
+        
+        char a2[] = b.toCharArray();
+        Arrays.sort(a2);
+        b =  new String(a2);
+        return a.equals(b);
+    }
+}
