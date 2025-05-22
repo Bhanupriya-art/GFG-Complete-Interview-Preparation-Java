7b763b5c0c8e60218c1642d7de307d5adf4f0788
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-05-22 12:32:33 +0530

    Create Deque_Implementation.java

---
+++ Deque - Libraries/Deque_Implementation.java
@@ -0,0 +1,22 @@
+
+public static void pb(ArrayDeque<Integer> dq, int x) {
+    //  code here
+    dq.addLast(x);
+}
+
+public static void ppb(ArrayDeque<Integer> dq) {
+    if(!dq.isEmpty()) dq.removeLast();
+    //  code here
+}
+
+    
+public static int front_dq(ArrayDeque<Integer> dq) {
+    //  code here
+    return dq.isEmpty() ? -1 : dq.peekFirst();
+}
+    
+
+public static void pf(ArrayDeque<Integer> dq, int x) {
+    //  code here
+    dq.addFirst(x);
+}
