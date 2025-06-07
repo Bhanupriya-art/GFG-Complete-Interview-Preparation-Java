e647f9d3df34654ea4fb4a09a26596f6b9cd8f68
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-06-07 15:05:07 +0530

    Create Unique_Substrings.java

---
+++ HashSet And LinkedHashSet - Libraries/Unique_Substrings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int unique_substring(String s) {
+        HashSet<String> set = new HashSet<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = i; j < n; j++) {
+                sb.append(s.charAt(j));
+                set.add(sb.toString());
+            }
+        }
+        return set.size();
+    }
+}
