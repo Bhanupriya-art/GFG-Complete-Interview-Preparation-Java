dfd271d8820423dfc55ba4b86f6dd90c082ee3e3
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-06-11 20:06:32 +0530

    Create HashMap_Operations.java

---
+++ HashMap And LinkedHashMap/HashMap_Operations.java
@@ -0,0 +1,17 @@
+class GfG {
+    void add_Value(HashMap<Integer, Integer> hm, int x, int y) {
+        hm.put(x, y);
+    }
+
+    int find_value(HashMap<Integer, Integer> hm, int x) {
+        return hm.getOrDefault(x, -1);
+    }
+
+    int getSize(HashMap<Integer, Integer> hm) {
+        return hm.size();
+    }
+
+    void removeKey(HashMap<Integer, Integer> hm, int x) {
+        hm.remove(x);
+    }
+}
