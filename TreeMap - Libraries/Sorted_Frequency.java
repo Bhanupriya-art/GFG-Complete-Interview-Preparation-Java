2859d4b197aa67ce563dfe51dd0db056361a607d
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-06-16 19:04:16 +0530

    Create Sorted_Frequency.java

---
+++ TreeMap - Libraries/Sorted_Frequency.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void freqSorted(int arr[]) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
