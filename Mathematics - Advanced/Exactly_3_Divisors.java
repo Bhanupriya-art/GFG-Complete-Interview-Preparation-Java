e8eda7a78f54a881f6e2b1b88b11297c5bc76050
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-07-23 17:48:11 +0530

    Create Exactly_3_Divisors.java

---
+++ Mathematics - Advanced/Exactly_3_Divisors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int exactly3Divisors(int n) {
+        int count = 0;
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime(i)) count++;
+        }
+        return count;
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
