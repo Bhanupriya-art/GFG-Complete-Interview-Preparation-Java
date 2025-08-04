38d324c06ee22e9c9b8633784238bf81efeb0884
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-08-04 18:03:20 +0530

    Create Gray_to_Binary_Conversion.java

---
+++ Bit Magic - Advanced/Gray_to_Binary_Conversion.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int grayToBinary(int n) {
+        int res = n;
+        while (n > 0) {
+            n = n >> 1;
+            res = res ^ n;
+        }
+        return res;
+    }
+}
