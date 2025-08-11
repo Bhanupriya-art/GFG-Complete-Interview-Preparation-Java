13fc0e352d6bd6726e8f12bb11b6a7716ef89194
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-08-11 21:29:19 +0530

    Create Power_Of_Numbers.java

---
+++ Recursion - Advanced/Power_Of_Numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseExponentiation(int n) {
+        int rev = 0, temp = n;
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+        return (int)Math.pow(n, rev);
+    }
+}
