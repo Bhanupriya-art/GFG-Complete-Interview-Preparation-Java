780a6163396490c4a91a85b786620e5644a40bc7
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-08-22 11:28:16 +0530

    Create Equilibrium_Point.java

---
+++ Arrays - Advanced/Equilibrium_Point.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int findEquilibrium(int arr[]) {
+        int total = 0, leftSum = 0;
+        for (int num : arr) total += num;
+        for (int i = 0; i < arr.length; i++) {
+            total -= arr[i];
+            if (leftSum == total) return i;
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+}
