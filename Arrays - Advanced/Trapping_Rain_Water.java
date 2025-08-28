f986eb548646df0dc386470c36cc04733799b90c
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-08-28 17:51:32 +0530

    Create Trapping_Rain_Water.java

---
+++ Arrays - Advanced/Trapping_Rain_Water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        int n = arr.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0, res = 0;
+        while (left < right) {
+            if (arr[left] <= arr[right]) {
+                if (arr[left] >= leftMax) leftMax = arr[left];
+                else res += leftMax - arr[left];
+                left++;
+            } else {
+                if (arr[right] >= rightMax) rightMax = arr[right];
+                else res += rightMax - arr[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
