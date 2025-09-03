09f9b41093367d275b2977a1fc8932aa7d710384
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-09-03 18:10:47 +0530

    Create Maximum_Sum_Subarray.java

---
+++ Arrays - Advanced/Maximum_Sum_Subarray.java
@@ -0,0 +1,10 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        int maxSoFar = arr[0], currMax = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            currMax = Math.max(arr[i], currMax + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currMax);
+        }
+        return maxSoFar;
+    }
+}
