d06fd3205a70eeea88e650f38657d19bcd05b634
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-09-22 16:04:31 +0530

    Create Closet_0s_1s_and_2s.java

---
+++ Sorting - Advanced/Closet_0s_1s_and_2s.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void segragate012(int arr[], int N) {
+        int low = 0, mid = 0, high = N - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+}
