ec6f8781b73a72c1297c6d17a278bfa8b38da96f
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-10-14 17:45:24 +0530

    Create Interchanging_the_rows_of_a_Matrix.java

---
+++ Matrices - Advanced/Interchanging_the_rows_of_a_Matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    static void interchangeRows(int matrix[][]) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - i - 1];
+            matrix[n - i - 1] = temp;
+        }
+    }
+}
