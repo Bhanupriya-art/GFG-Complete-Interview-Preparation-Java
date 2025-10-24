a372ec20d710239c3984b8a5ab981b0bb2465062
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-10-24 18:52:16 +0530

    Create Intersection_of_Arrays_with_Distinct.java

---
+++ Hashing - Advanced/Intersection_of_Arrays_with_Distinct.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int intersectSize(int a[], int b[]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : a) set.add(num);
+        int count = 0;
+        for (int num : b) {
+            if (set.contains(num)) {
+                count++;
+                set.remove(num);
+            }
+        }
+        return count;
+    }
+}
