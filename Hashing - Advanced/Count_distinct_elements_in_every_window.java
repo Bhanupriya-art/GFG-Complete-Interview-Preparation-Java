b471f9a3082b1dca05ec663673ee2701c1210567
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-10-29 18:42:19 +0530

    Create Count_distinct_elements_in_every_window.java

---
+++ Hashing - Advanced/Count_distinct_elements_in_every_window.java
@@ -0,0 +1,18 @@
+class Solution {
+    ArrayList<Integer> countDistinct(int arr[], int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k; i++)
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        res.add(map.size());
+        for (int i = k; i < arr.length; i++) {
+            if (map.get(arr[i - k]) == 1)
+                map.remove(arr[i - k]);
+            else
+                map.put(arr[i - k], map.get(arr[i - k]) - 1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            res.add(map.size());
+        }
+        return res;
+    }
+}
