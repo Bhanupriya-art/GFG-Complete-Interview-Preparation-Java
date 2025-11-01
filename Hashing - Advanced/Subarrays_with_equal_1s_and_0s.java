4937ddfcbbb8b5726dfc7a7100dfb9b0cb78e5c8
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-11-01 14:28:42 +0530

    Subarrays_with_equal_1s_and_0s.java

---
+++ Hashing - Advanced/Subarrays_with_equal_1s_and_0s.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSubarray(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0, count = 0;
+        map.put(0, 1);
+        for (int num : arr) {
+            sum += (num == 0 ? -1 : 1);
+            if (map.containsKey(sum)) count += map.get(sum);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
