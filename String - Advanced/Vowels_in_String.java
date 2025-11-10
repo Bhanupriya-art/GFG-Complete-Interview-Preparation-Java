3b5abed5a246c801c179839bec5954b5996a068d
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-11-10 21:18:05 +0530

    Create Vowels_in_String.java

---
+++ String - Advanced/Vowels_in_String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int countVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') count++;
+        }
+        return count;
+    }
+}
