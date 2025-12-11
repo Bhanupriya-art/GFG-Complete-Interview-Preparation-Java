bbadba7957ed9f287b4392e40cac5a01a8cbfddf
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-12-11 15:07:59 +0530

    Create Sum_of_numbers_in_string.java

---
+++ String - Advanced/Sum_of_numbers_in_string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findSum(String s) {
+        int sum = 0, num = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else {
+                sum += num;
+                num = 0;
+            }
+        }
+        return sum + num;
+    }
+}
